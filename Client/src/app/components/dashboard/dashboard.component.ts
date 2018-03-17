2e3843a76a9dfd63c6422520a11cf4515de3c693
mtclark0518 <mtclark0518@gmail.com>
2018-03-16 22:01:39 -0600

    starts reformatting for mobile

--- Client/src/app/components/dashboard/dashboard.component.ts
+++ Client/src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  type: string;
   constructor() { }
 
   ngOnInit() {
+    this.type = 'seeking';
   }
-
+  setType(type) {
+    this.type = type;
+  }
 }
